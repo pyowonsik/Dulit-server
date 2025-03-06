f11b4046fba306d1d60a7cdd28fa8f97fedf64c7
pyo_16 <94816219+pyowonsik@users.noreply.github.com>
2025-03-06 13:32:31 +0900

    feat : send chat to chatRoom

--- apps/chat/src/chat/chat.service.ts
+++ apps/chat/src/chat/chat.service.ts
@@ -5,12 +5,17 @@ import { CreateChatRoomDto } from './dto/create-chat-room.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Socket } from 'socket.io';
+import { Chat } from './schema/chat.schema';
+import { CreateChatDto } from './dto/create-chat.dto';
+import { plainToClass } from 'class-transformer';
 
 @Injectable()
 export class ChatService {
   constructor(
     @InjectModel(ChatRoom.name)
     private readonly chatroomModel: Model<ChatRoom>,
+    @InjectModel(Chat.name)
+    private readonly chatModel: Model<Chat>,
   ) {}
 
   private readonly connectedClients = new Map<string, Socket>();
@@ -23,6 +28,7 @@ export class ChatService {
     this.connectedClients.delete(userId);
   }
 
+  // 채팅방 생성성
   async createChatRoom(dto: CreateChatRoomDto) {
     const chatroom = await this.chatroomModel.create({
       user1Id: dto.user1Id,
@@ -33,15 +39,43 @@ export class ChatService {
     return chatroom;
   }
 
-  async joinUserRooms(user: { sub: number }, client: Socket) {
-    // const chatRooms = await this.findMyChatRoom(user.sub);
-    // if (chatRooms.length === 0) {
-    //   client.emit('error', { message: '커플이 매칭 되지 않은 사용자입니다.' });
-    //   return;
-    // }
-    // // socket Connect -> client(socket) chatRooms의 id 값으로 join
-    // chatRooms.forEach((room) => {
-    //   client.join(room.id.toString());
-    // });
+  async joinUserRooms(user: { sub: string }, client: Socket) {
+    const chatRoom = await this.findMyChatRoom(user.sub);
+
+    if (!chatRoom) {
+      client.emit('error', { message: '커플이 매칭 되지 않은 사용자입니다.' });
+      return;
+    }
+
+    console.log(`Join chat room - ${chatRoom._id.toString()}`);
+    client.join(chatRoom._id.toString());
+  }
+
+  // 채팅 메세지 전송
+  async sendMessage(payload: { sub: string }, { message }: CreateChatDto) {
+    const chatRoom = await this.findMyChatRoom(payload.sub);
+
+    const chatModel = await this.chatModel.create({
+      userId: payload.sub,
+      message: message,
+      chatRoomId: chatRoom,
+    });
+
+    const client = this.connectedClients.get(payload.sub);
+
+    const roomId = chatRoom._id.toString();
+
+    // MongoDB는 반환된 객체가 이미 plain object
+    client.to(roomId).emit('sendMessage', chatModel);
+
+    return message;
+  }
+
+  async findMyChatRoom(userId: string) {
+    const chatRoom = await this.chatroomModel.findOne({
+      $or: [{ user1Id: userId }, { user2Id: userId }],
+    });
+
+    return chatRoom;
   }
 }
