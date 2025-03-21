4cc9e05e3a7201ef51c795e35f1d4416841d2f58
pyo_16 <94816219+pyowonsik@users.noreply.github.com>
2025-03-21 16:44:25 +0900

    refactor : cicd 테스트 주석

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 async function bootstrap() {
   // jenkins 테스트 주석
   // jenkins 테스트 주석
+  // jenkins 테스트 주석
 
   const app = await NestFactory.create(AppModule, {
     logger: ['verbose'],
