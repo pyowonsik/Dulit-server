07b794a5bf6500f51ed26093ae21c7b75fdaa65c
pyo_16 <94816219+pyowonsik@users.noreply.github.com>
2025-03-24 13:19:46 +0900

    refactor : cicd 배포 테스트

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 async function bootstrap() {
   // CICD 주석 테스트
   // CICD 주석 테스트
+  // CICD 주석 테스트
 
   const app = await NestFactory.create(AppModule, {
     logger: ['verbose'],
